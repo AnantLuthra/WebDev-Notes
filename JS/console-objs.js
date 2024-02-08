fed96a5d047f2eae10561cd56124d36f661cd13d
Anant <anant312004@gmail.com>
2024-02-08 10:28:58 +0530

    learnt about console objects

---
+++ JS/console-objs.js
@@ -0,0 +1,20 @@
+
+let a = {
+    "anant": 19,
+    "shiv": 18,
+    "aastha": 10,
+    "shivang": 11
+}
+
+console.table(
+    a
+)
+
+console.error("bhag ja yaha se")
+console.info("bahut safai h yaha")
+
+console.time("hey")
+for (let i in a){
+    console.log()
+}
+console.timeEnd("hey")
